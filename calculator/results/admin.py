c85281dc3d296e61e60aa6799df7bde3fdde34ae
udipta <iamudi03@gmail.com>
2021-09-27 20:54:09 +0530

    add model class and app config

--- calculator/results/admin.py
+++ calculator/results/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from results import models
+
+admin.site.register(models.Results)
--- calculator/results/models.py
+++ calculator/results/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Results(models.Model):
+    # The fields in the database are number1, number2 and answer.
+    number1 = models.CharField(max_length=512, blank=False, null=False)
+    number2 = models.CharField(max_length=512, blank=False, null=False)
+    answer = models.CharField(max_length=512, blank=True, null=True)
+
+    def __str__(self):
+        return f"Number1: {self.number1} + Number2: {self.number2} = {self.answer}"
